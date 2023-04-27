0f985b4e65e042d4d065999cf5e1398a9fd6d27c
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-04-27 17:54:05 -0300

    update

--- PontoEletronico/src/components/modal.js
+++ PontoEletronico/src/components/modal.js
@@ -1,46 +1,68 @@
-export function tst() {
-    let component = document.createElement('section');
-    let src = document.createElement('div');
-    let img = document.createElement('img');
-    let content = document.createElement('section');
-    let title = document.createElement('div');
-    let h1 = document.createElement('h1');
-    let description = document.createElement('div');
-    let p = document.createElement('p');
 
-    component.setAttribute('class','component')
-    src.setAttribute('class','src');
-    img.setAttribute('class','srcModal')
-    content.setAttribute('class','content')
-    title.setAttribute('class','divTitle')
-    h1.setAttribute('id','title')
-    description.setAttribute('class','divDescription')
-    p.setAttribute('id','description')
+export function modal(texth1, textp) {
+    const main = document.querySelector("#main");
+    const component = document.createElement('section');
+    const src = document.createElement('div');
+    const img = document.createElement('img');
+    const content = document.createElement('section');
+    const title = document.createElement('div');
+    const h1 = document.createElement('h1');
+    const description = document.createElement('div');
+    const p = document.createElement('p');
+    const button = document.createElement('button');
+    const closeModal = document.createElement('p');
+
+    main.appendChild(component);
+    component.setAttribute('class', 'component')
+    closeModal.innerHTML = '&times;'
+    closeModal.setAttribute('class', 'closeModal');
+    src.setAttribute('class', 'src');
+    img.setAttribute('class', 'srcModal');
+    img.setAttribute('src', 'https://pbs.twimg.com/media/EeieACpUYAAVJTD.png');
+    content.setAttribute('class', 'content')
+    title.setAttribute('class', 'divTitle')
+    h1.setAttribute('id', 'title');
+    h1.innerHTML = texth1;
+    description.setAttribute('class', 'divDescription')
+    p.setAttribute('id', 'description');
+    p.innerHTML = textp;
+    button.setAttribute('type', 'button');
+    button.setAttribute('class', 'btnModal');
+    button.innerHTML = 'ok';
+
 
     component.appendChild(src);
+    src.appendChild(closeModal);
     src.appendChild(img);
     component.appendChild(content);
     content.appendChild(title);
     title.appendChild(h1);
     content.appendChild(description);
     description.appendChild(p);
+    content.appendChild(button)
 
     console.log(component)
-}
+    document.body.appendChild(main)
 
-/**
- *    <section class="component">
-            <div class="src">
-                <img src="https://via.placeholder.com/100" alt="logomark" id="srcModal">
-            </div>
-            <div class="content">
-                <div class="title">
-                    <h1 id="title"></h1>
-                </div>
-                <div class="description">
-                    <p id="description"></p>
-                </div>
-            </div>
-        </section>
-     
- */
+
+    
+    $(".closeModal").on({
+        click:()=>{
+            component.style.transition='1s';
+            component.style.opacity='0';
+            setTimeout(()=>{
+                component.style.display='none';
+            },1000)
+        }
+    }) ;
+    $(".btnModal").on({
+        click:()=>{
+            component.style.transition='1s';
+            component.style.opacity='0';
+            setTimeout(()=>{
+                component.style.display='none';
+            },1000)
+        }
+    })
+    
+}
--- PontoEletronico/src/js/index.js
+++ PontoEletronico/src/js/index.js
@@ -1,6 +1,24 @@
-import { tst } from "../components/modal.js";
+import { modal } from "../components/modal.js";
 $("#btnLogin").on({
-    click:()=>{
-        tst()
+    click: () => {
+        const login = $("#fieldLogin").val();
+        const storageLogin = localStorage.getItem('login');
+
+        const pass = $("#fieldPassword").val();
+        const storagePass = localStorage.getItem('pass');
+
+
+        if (login == storageLogin && pass == storagePass) {
+            modal('Sucesso','Em breve você será redirecionado para o painel principal');
+            setTimeout(()=>{
+                window.location.href='./dashboard/adminDashboard.html';
+            },1000)
+        }
     }
 })
+
+setTimeout(() => {
+    localStorage.setItem('login', 'admin');
+    localStorage.setItem('pass', '123');
+}, 1000);
+
