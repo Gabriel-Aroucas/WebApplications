be570ca9080e975d01aa71398f1eff71305499b2
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-04-27 15:56:47 -0300

    update

--- PontoEletronico/src/components/modal.js
+++ PontoEletronico/src/components/modal.js
@@ -1,4 +1,31 @@
 export function tst() {
+    let component = document.createElement('section');
+    let src = document.createElement('div');
+    let img = document.createElement('img');
+    let content = document.createElement('section');
+    let title = document.createElement('div');
+    let h1 = document.createElement('h1');
+    let description = document.createElement('div');
+    let p = document.createElement('p');
+
+    component.setAttribute('class','component')
+    src.setAttribute('class','src');
+    img.setAttribute('class','srcModal')
+    content.setAttribute('class','content')
+    title.setAttribute('class','divTitle')
+    h1.setAttribute('id','title')
+    description.setAttribute('class','divDescription')
+    p.setAttribute('id','description')
+
+    component.appendChild(src);
+    src.appendChild(img);
+    component.appendChild(content);
+    content.appendChild(title);
+    title.appendChild(h1);
+    content.appendChild(description);
+    description.appendChild(p);
+
+    console.log(component)
 }
 
 /**
