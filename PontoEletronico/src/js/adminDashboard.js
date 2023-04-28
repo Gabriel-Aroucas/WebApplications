95d894e74694eeb664ba34d9ece0e4e5eb9f70f1
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-04-27 21:30:46 -0300

    update

--- PontoEletronico/src/js/adminDashboard.js
+++ PontoEletronico/src/js/adminDashboard.js
@@ -1,1 +1,35 @@
-"use strict";
+//import {modal} from "../components/modal.js";
+
+setInterval(() => {
+    let data = new Date();
+
+    let day = data.getDate();
+    let month = data.getMonth(); month+=1;
+    let year = data.getFullYear();
+    
+    let hour = data.getHours();
+    let minutes = data.getMinutes();
+    let seconds = data.getSeconds();
+
+
+    
+    $("#date").text(day + '/' + month + '/' + year)
+    $("#hours").text(hour + ':' + minutes + ':' + seconds)
+
+    if(hour >= 0 && hour<=4){
+        $("#luminous").text('Boa Madrugada');
+     }else if (hour >= 5 && hour<=11){
+        $("#luminous").text('Bom Dia');
+     }else if (hour >= 12 && hour<=18){
+        $("#luminous").text('Boa Tarde');
+     }else{
+        $("#luminous").text('Boa Noite');
+     }
+
+}, 1000)
+
+$("#pointerRegister").on({
+    click:()=>{
+        modal('Ponto Registrado','Você pode acompanhar na seção Histórico')
+    }
+})
--- PontoEletronico/src/js/index.js
+++ PontoEletronico/src/js/index.js
@@ -12,7 +12,14 @@ $("#btnLogin").on({
             modal('Sucesso','Em breve você será redirecionado para o painel principal');
             setTimeout(()=>{
                 window.location.href='./dashboard/adminDashboard.html';
-            },1000)
+            },2000);
+        }else{
+            if(login.length == 0 || pass.length == 0){
+                modal('Dados incompletos','Preencha os dados de login')
+            }else{
+                modal('Dados incorretos','Login ou senha incorretos')
+
+            }
         }
     }
 })
